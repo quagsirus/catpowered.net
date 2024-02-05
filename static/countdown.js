da022699b8c0c05ad1dc83e04569aa2662261d77
quagsirus <quagsirus@catpowered.net>
2024-02-05 04:23:09 +0000

    remake time remaining logic

--- static/countdown.js
+++ static/countdown.js
@@ -11,19 +11,57 @@ var x = setInterval(function() {
         var distance = countDownDate - now;
         var prefix = timers[timer_pos].getAttribute("prefix")
 
-        // Time calculations for days, hours, minutes and seconds
+        // Time calculations for years, months, weeks, days, hours, minutes, and seconds
         var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365));
-        var days = Math.floor(distance % (1000 * 60 * 60 * 24 * 365) / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        var remainingTime = distance % (1000 * 60 * 60 * 24 * 365);
+
+        var months = Math.floor(remainingTime / (1000 * 60 * 60 * 24 * 30)); // Assuming a month is 30 days
+        remainingTime %= (1000 * 60 * 60 * 24 * 30);
+
+        var weeks = Math.floor(remainingTime / (1000 * 60 * 60 * 24 * 7));
+        remainingTime %= (1000 * 60 * 60 * 24 * 7);
+
+        var days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+        remainingTime %= (1000 * 60 * 60 * 24);
+
+        var hours = Math.floor(remainingTime / (1000 * 60 * 60));
+        remainingTime %= (1000 * 60 * 60);
+
+        var minutes = Math.floor(remainingTime / (1000 * 60));
+        remainingTime %= (1000 * 60);
+
+        var seconds = Math.floor(remainingTime / 1000);
 
         // Display the result in the element
+        var remainingTimeStr = prefix;
+
         if (years > 0) {
-            timers[timer_pos].innerHTML = prefix + years + "y " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-        } else {
-            timers[timer_pos].innerHTML = prefix + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+            remainingTimeStr += years + "y ";
         }
+
+        if (months > 0 || years > 0) {
+            remainingTimeStr += months + "mo ";
+        }
+
+        if (weeks > 0 || months > 0 || years > 0) {
+            remainingTimeStr += weeks + "w ";
+        }
+
+        if (days > 0 || weeks > 0 || months > 0 || years > 0) {
+            remainingTimeStr += days + "d ";
+        }
+
+        if (hours > 0 || days > 0 || weeks > 0 || months > 0 || years > 0) {
+            remainingTimeStr += hours + "h ";
+        }
+
+        if (minutes > 0 || hours > 0 || days > 0 || weeks > 0 || months > 0 || years > 0) {
+            remainingTimeStr += minutes + "m ";
+        }
+
+        remainingTimeStr += seconds + "s";
+
+        timers[timer_pos].innerHTML = remainingTimeStr;
 
         // If the count down is finished, write some text
         if (distance < 0) {
